f145e800f86f9f71b26624b8b86aa62b45701d76
paul loncke <pauloncke@gmail.com>
2021-08-10 15:28:54 +0200

    success

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,42 @@
+# frozen_string_literal: true
+
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit; end
+
+  def update
+    @task.update(restaurant_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
